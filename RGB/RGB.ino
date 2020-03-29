501c672f971eb716b3a169cff32747091768e5a4
Faruk Hossain1807142 <farukfari24@gmail.com>
2020-03-29 14:00:05 +0600

    Comment Added

--- RGB/RGB.ino
+++ RGB/RGB.ino
@@ -1,17 +1,24 @@
-int redPin=2;
-int greenPin=3;
-int bluePin=4;
+/*
+ This code is base on an led which name is RGB.
+ This led can creat any type of colour. 
+ In this code I will make an code which can turn on any type of light 
+ */
+ 
+
+int redPin=2; // Integer of pin for RED line of RGB 
+int greenPin=3; // Integer of pin for GREEN line of RGB
+int bluePin=4; // Integer of pin for blue line of RGB
 
 void setup()
 {
-  pinMode(redPin,OUTPUT);
-  pinMode(greenPin,OUTPUT);
-  pinMode(bluePin,OUTPUT);
+  pinMode(redPin,OUTPUT); // pin mode for Red line of RGB 
+  pinMode(greenPin,OUTPUT); // pin mode for green line of RGB
+  pinMode(bluePin,OUTPUT); // pin mode for Blue line of RGB
 }
 
 void loop()
 {
-  digitalWrite(redPin,HIGH);
-  digitalWrite(greenPin,LOW);
-  digitalWrite(bluepin,LOW);
+  digitalWrite(redPin,HIGH); // Turn ON RED line of RGB
+  digitalWrite(greenPin,LOW); // Turn OFF green line of RGB
+  digitalWrite(bluepin,LOW); // Turn OFF Blue line of RGB
 }
