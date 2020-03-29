c2a8c8a30b18b0cd1fd0ddc4c011b4ded586990b
Faruk Hossain1807142 <farukfari24@gmail.com>
2020-03-29 13:26:57 +0600

    Comment added on blink

--- blink/blink.ino
+++ blink/blink.ino
@@ -1,15 +1,19 @@
-int pinNum=13;
-int delTime=1000;
+/*This code only for led on arduino which is 
+ added to pin 13. 
+ For blink the light */
+ 
+int pinNum=13; // Creating an integer for pin 13
+int delTime=1000; // Creating an integer for delay time
 
 void setup(){
-  pinMode(pinNum,OUTPUT);
+  pinMode(pinNum,OUTPUT); // pin mode for pin 13
   
 }
 
 void loop(){
-  digitalWrite(pinNum,HIGH);
-  delay(delTime);
-  digitalWrite(pinNum,LOW);
-  delay(delTime);
+  digitalWrite(pinNum,HIGH); // turn ON the led
+  delay(delTime); // wait for 1 seconds 
+  digitalWrite(pinNum,LOW); // turn OFF the led
+  delay(delTime); // OFF for 1 seconds 
   
 }
